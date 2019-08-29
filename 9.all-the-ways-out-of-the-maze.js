90a630d00760e34e8bad04782fcb1b0920aca978
Artan Muzhaqi <artanmuzhaqi@gmail.com>
2019-08-29 14:41:26 -0400

    working on all the possible ways

--- 9.all-the-ways-out-of-the-maze.js
+++ 9.all-the-ways-out-of-the-maze.js
@@ -1,68 +1,80 @@
 let maze = [
     [' ', ' ', ' ', '*', ' ', ' ', ' '],
     ['*', '*', ' ', '*', ' ', '*', ' '],
-    [' ', ' ', ' ', ' ', ' ', ' ', ' '],
-    [' ', '*', '*', '*', '*', '*', ' '],
+    [' ', ' ', ' ', ' ', ' ', '*', ' '],
+    [' ', '*', '*', '*', '*', '*', '*'],
     [' ', ' ', ' ', ' ', ' ', ' ', 'e']
 ];
 let mySmallMaze = [
-    [' ', ' ', ' '],
-    [' ', ' ', '*'],
-    [' ', '*', 'e']
+    [' ', ' ', ' ', ' '],
+    [' ', '*', ' ', '*'],
+    [' ', '*', ' ', ' '],
+    [' ', '*', '*', '*'],
+    [' ', ' ', ' ', 'e']
 ];
 const findPath = (x, y, maze) => {
     const xSize = maze.length;
     const ySize = maze[0].length;
     //check if it is within the range of array size
-    if ((x >= xSize || x < 0) || (y >= ySize || y < 0))
+    if ((x > xSize || x < 0) || (y > ySize || y < 0))
         return false
     else if (maze[x][y] === "e")
         return true
     //check if it is blocked
     else if (maze[x][y] === "*")
         return false
-    //check if it is open
+    //check if it is openou
     else if (maze[x][y] === " ")
         return true
 }
 const checkNext = (maze, x, y) => {
     //check if it is the exit
+    let dir = []
     if (maze[x][y] === "e")
-        return 'E'
+        dir.push('E')
     //check if it can go right
-    else if (findPath(x, y + 1, maze))
-        return 'R'
+    if (findPath(x, y + 1, maze))
+        dir.push('R')
     //check if it can go down
-    else if (findPath(x + 1, y, maze))
-        return 'D'
+    if (findPath(x + 1, y, maze))
+        dir.push('D')
     //check if it can go up
-    else if (findPath(x, y - 1, maze))
-        return 'L'
+    if (findPath(x - 1, y, maze))
+        dir.push('U')
     //check if it can go left
-    else if (findPath(x - 1, y, maze))
-        return 'U'
-    return false;
+    if (findPath(x, y - 1, maze))
+        dir.push('L')
+    return dir;
 }
 const outOfMaze = (maze, currentX = 0, currentY = 0, path = []) => {
     const direction = checkNext(maze, currentX, currentY)
-    if (!direction) {
+
+    if (!direction.length === 0) {
         return 'There is no way out of the maze'
     }
-    if (direction === 'E')
-        return path.join("")
-    else if (direction === 'R')
-        currentY += 1;
-    else if (direction === 'D')
-        currentX += 1;
-    else if (direction === 'U') {
-        maze[currentX][currentY] = "*"
-        currentX -= 1;
-    }
-    else if (direction === 'L') {
-        maze[currentX][currentY] = "*"
-        currentY -= 1;
-    }
-    path.push(direction)
+    const result = direction.map(dir => {
+        if (dir === 'E')
+            return path.join("")
+        else if (dir === 'R') {
+            path.push('R')
+            currentY += 1;
+        }
+        else if (dir === 'D') {
+            path.push('D')
+            currentX += 1;
+        }
+        else if (dir === 'U') {
+            path.push('U')
+            currentX -= 1;
+        }
+        else if (dir === 'L') {
+            path.push('L')
+            currentY -= 1;
+        }
+        maze[currentX][currentY] = '*'
+        return outOfMaze(maze, currentX, currentY, path)
+    })
+    console.log(result)
     return outOfMaze(maze, currentX, currentY, path)
 }
 outOfMaze(mySmallMaze)
