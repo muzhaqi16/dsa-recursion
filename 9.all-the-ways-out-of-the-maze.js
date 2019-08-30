fc16f0003d2e4e6881ef133f141a822edb4e3772
Artan Muzhaqi <artanmuzhaqi@gmail.com>
2019-08-29 20:50:42 -0400

    Advanced solution for one way

--- 9.all-the-ways-out-of-the-maze.js
+++ 9.all-the-ways-out-of-the-maze.js
@@ -1,10 +1,3 @@
-let maze = [
-    [' ', ' ', ' ', '*', ' ', ' ', ' '],
-    ['*', '*', ' ', '*', ' ', '*', ' '],
-    [' ', ' ', ' ', ' ', ' ', '*', ' '],
-    [' ', '*', '*', '*', '*', '*', '*'],
-    [' ', ' ', ' ', ' ', ' ', ' ', 'e']
-];
 let mySmallMaze = [
     [' ', ' ', ' ', ' '],
     [' ', '*', ' ', '*'],
@@ -15,66 +8,45 @@ let mySmallMaze = [
 const findPath = (x, y, maze) => {
     const xSize = maze.length;
     const ySize = maze[0].length;
-    //check if it is within the range of array size
-    if ((x > xSize || x < 0) || (y > ySize || y < 0))
+    if ((x >= xSize || x < 0) || (y >= ySize || y < 0))
         return false
     else if (maze[x][y] === "e")
         return true
-    //check if it is blocked
     else if (maze[x][y] === "*")
         return false
-    //check if it is openou
     else if (maze[x][y] === " ")
         return true
 }
-const checkNext = (maze, x, y) => {
-    //check if it is the exit
-    let dir = []
-    if (maze[x][y] === "e")
-        dir.push('E')
-    //check if it can go right
-    if (findPath(x, y + 1, maze))
-        dir.push('R')
-    //check if it can go down
-    if (findPath(x + 1, y, maze))
-        dir.push('D')
-    //check if it can go up
-    if (findPath(x - 1, y, maze))
-        dir.push('U')
-    //check if it can go left
-    if (findPath(x, y - 1, maze))
-        dir.push('L')
-    return dir;
-}
-const outOfMaze = (maze, currentX = 0, currentY = 0, path = []) => {
-    const direction = checkNext(maze, currentX, currentY)
-
-    if (!direction.length === 0) {
-        return 'There is no way out of the maze'
+const checkNext = (maze, x, y, dir = []) => {
+    if (maze[x][y] === 'e')
+        return
+    if (findPath(x, y + 1, maze)) {
+        dir.push('R');
+        checkNext(maze, x, y + 1, dir);
     }
-    const result = direction.map(dir => {
-        if (dir === 'E')
-            return path.join("")
-        else if (dir === 'R') {
-            path.push('R')
-            currentY += 1;
-        }
-        else if (dir === 'D') {
-            path.push('D')
-            currentX += 1;
-        }
-        else if (dir === 'U') {
-            path.push('U')
-            currentX -= 1;
-        }
-        else if (dir === 'L') {
-            path.push('L')
-            currentY -= 1;
-        }
-        maze[currentX][currentY] = '*'
-        return outOfMaze(maze, currentX, currentY, path)
-    })
-    console.log(result)
-    return outOfMaze(maze, currentX, currentY, path)
+    else if (findPath(x + 1, y, maze)) {
+        dir.push('D')
+        checkNext(maze, x + 1, y, dir)
+    }
+    else if (findPath(x, y - 1, maze)) {
+        dir.push('L')
+        maze[x][y] = '*'
+        checkNext(maze, x, y - 1, dir)
+    }
+    else if (findPath(x - 1, y, maze)) {
+        dir.push('U')
+        maze[x][y] = '*'
+        checkNext(maze, x - 1, y, dir)
+    }
+    return dir
+}
+checkDirections = (maze, x = 0, y = 0) => {
+    if (maze[x][y] === 'e')
+        return true
+    return checkNext(maze, x, y)
+
+}
+outOfMaze = (maze) => {
+    console.log(checkDirections(maze))
 }
 outOfMaze(mySmallMaze)
